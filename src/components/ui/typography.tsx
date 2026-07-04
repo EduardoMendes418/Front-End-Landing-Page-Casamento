2bc61bbfdef336e15c9b616d44057d86cd3c96f8
eduardo <eduardomendes418@gmail.com>
2026-07-04 17:30:17 -0300

    [72-CAS] feat: Typography

--- src/components/ui/typography.tsx
+++ src/components/ui/typography.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { cn } from "@/lib/utils";
+import { cn } from "@/utils";
 
 type TypographyVariant = "h1" | "h2" | "h3" | "h4" | "p" | "lead" | "small" | "muted";
 
